4f55608fdb1f80f550dbe8e7c1de7bb4f635da95
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-10-20 16:23:31 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.use(express.urlencoded({ extended: true }));  // POST METHOD
 app.use(cookieParser());
 
 
-// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 
 // ROUTES
