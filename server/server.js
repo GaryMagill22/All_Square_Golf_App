dc63df93f49f2e0102c28cf764d2c94846771c72
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-11-05 15:16:12 -0700

    added .env to client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 // Load environment variables first
-// const envPath = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-// require('dotenv').config({ path: envPath });
+const envPath = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+require('dotenv').config({ path: envPath });
 
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
