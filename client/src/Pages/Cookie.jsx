4b531c30a140eda38eaeb044882a6410363fe264
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-11-05 14:37:05 -0700

    changed cookie call to not have double api

--- client/src/Pages/Cookie.jsx
+++ client/src/Pages/Cookie.jsx
@@ -8,13 +8,13 @@ import { useEffect } from 'react';
 
 
 
-
+// CHANGED THE URL FROM APIURL/API/COOKIE TO APIURL/COOKIE
 const Cookie = () => {
     // Deployment & Local using env variables
     useEffect(() => {
         const apiURL = process.env.REACT_APP_API_URL;
         console.log('cookie - apiURL: ', apiURL);
-        axios.get(`${apiURL}/api/cookie`, { withCredentials: true })
+        axios.get(`${apiURL}/cookie`, { withCredentials: true })
             .then(res => console.log("success!"))
             .catch(err => console.log(err));
     }, [])
