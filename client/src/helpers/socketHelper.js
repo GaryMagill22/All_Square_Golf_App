ec5fa3b5a6784be10c662d244059113ace660c44
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-11-06 13:53:49 -0700

    comit

--- client/src/helpers/socketHelper.js
+++ client/src/helpers/socketHelper.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export const initSocket = () => {
     if (!socket) {
-        socket = io('wss://allsquare.club:8000');
+        socket = io('wss://allsquare.club:');  
 
         socket.on('connect', () => {
             console.log('Connected to server');
