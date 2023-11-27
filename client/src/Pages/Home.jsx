04bb1e015dcf7144a42d5ea6e66d6e18e2ed72c4
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-11-27 16:59:53 -0700

    home.jsx btn changed from lightblue to cyan-normal

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -153,8 +153,6 @@ const Home = () => {
     const handleJoinRoom = async (lobbyId) => {
         const inputLobbyId = document.getElementById('lobbyIdInput').value;
 
-        // Close the modal
-        // setIsModalOpen(false);
 
         // Use a timeout or a callback to wait for the modal to finish its closing animation (if any).
         setTimeout(() => {
@@ -197,7 +195,7 @@ const Home = () => {
                         {({ open }) => (
                             <>
                                 <div className="relative mt-2">
-                                    <Listbox.Button className="relative w-full cursor-default mb-2 rounded-md bg-blue-light py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    <Listbox.Button className="relative w-full cursor-default mb-2 rounded-md bg-cyan-normal py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                         <span className="block truncate">{selectedGame ? selectedGame.name : 'Select Game'}</span>
                                         <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                                             <ChevronUpDownIcon className="h-5 w-5 text-black" aria-hidden="true" />
@@ -250,7 +248,7 @@ const Home = () => {
                             <>
                                 {/* <Listbox.Label className="block text-sm font-medium leading-6 text-white mt-1">Select Course:</Listbox.Label> */}
                                 <div className="relative mt-2">
-                                    <Listbox.Button className="relative w-full cursor-default rounded-md bg-blue-light py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    <Listbox.Button className="relative w-full cursor-default rounded-md bg-cyan-normal py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                         <span className="block truncate">{selectedCourse ? selectedCourse.name : 'Select Course'}</span>
                                         <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                                             <ChevronUpDownIcon className="h-5 w-5 text-black" aria-hidden="true" />
