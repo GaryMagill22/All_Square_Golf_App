c1a39f0ba2a99f9e6d4bb2951bedcc7f40cc3872
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-12-04 11:01:37 -0700

    updated payment verification image

--- client/src/Pages/VerifyPayment.jsx
+++ client/src/Pages/VerifyPayment.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import paymentVerifyImage from "../assets/payment-verify.jpeg";
+
 
 
 export default function VerifyPayment() {
     return (
         <div>
             <div>
-                <img src={require("../assets/payment-verify.jpeg")} alt="Verify Payment" />
+                <img src={paymentVerifyImage} alt="Verify Payment" />
                 <h2>Payment is being verified...Your wallet will be funded once payment is confirmed.</h2>
                 <Link to="/home">Go Back Home</Link>
             </div>
