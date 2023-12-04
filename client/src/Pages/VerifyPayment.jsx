8588bd54cf7e3e7674c3a9b82c0dfdd9904f5ec4
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-12-04 13:57:40 -0700

    updated deposit verification

--- client/src/Pages/VerifyPayment.jsx
+++ client/src/Pages/VerifyPayment.jsx
@@ -4,13 +4,20 @@ import paymentVerifyImage from "../assets/payment-verify.jpeg";
 
 
 
+
+
 export default function VerifyPayment() {
     return (
-        <div>
+        <div >
             <div>
                 <img src={paymentVerifyImage} alt="Verify Payment" />
-                <h2>Payment is being verified...Your wallet will be funded once payment is confirmed.</h2>
-                <Link to="/home">Go Back Home</Link>
+                <h2>Payment is verified - Deposit successful! Return to Profile to see funds.</h2>
+                <Link to="/profile" className="w-40 inline-flex justify-center mt-4 py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-cyan-normal hover:bg-indigo-dark hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-salmon-light">
+                    Profile
+                </Link>
+                <Link to="/home" className="w-40 inline-flex justify-center mt-4 py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-maroon-normal hover:bg-indigo-dark hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-salmon-light">
+                    Home
+                </Link>
             </div>
         </div>
     )
