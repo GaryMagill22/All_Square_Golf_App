ac62105d97fad1859af1445cc9674a89390c87dc
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-12-05 17:03:17 -0700

    removed splash png import

--- client/src/Pages/DashBoard.jsx
+++ client/src/Pages/DashBoard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/AllSquareLogo-WhiteNoBackground.png';
-import golfBallGraphic from '../assets/GolfBallSplash.png';
 
 
 const DashBoard = () => {
